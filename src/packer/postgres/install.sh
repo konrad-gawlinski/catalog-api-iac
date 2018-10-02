9d91c246b2d443f64af501496b9327db5ff2374e
Konrad Gawlinski <konrad.gawlinski@gmail.com>
2018-10-02 22:56:06 +0200

    Prepare packer image for postgres

--- src/packer/postgres/install.sh
+++ src/packer/postgres/install.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
+apt-get update
+apt-get install -y wget unzip ca-certificates apt-transport-https
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O-  | apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" | tee -a /etc/apt/sources.list.d/php.list
+
+apt-get update
+apt-get -y install supervisor postgresql-10
+
+rm -rf /var/lib/postgresql/10/main
+userdel -r postgres
+
+echo 'export PGDATA=/var/pgsql_data' | tee /etc/profile
+adduser --disabled-password --gecos "" postgres-admin
+mkdir -p /var/pgsql_data && chown postgres-admin:postgres-admin /var/pgsql_data && chmod 770 /var/pgsql_data
+mkdir -p /var/log/postgres && chown postgres-admin:postgres-admin /var/log/postgres && chmod 774 /var/log/postgres
+chown -R postgres-admin:postgres-admin /var/run/postgresql
+sed -i '/en_US\.UTF-8 UTF-8/s/^#//' /etc/locale.gen && locale-gen
+su -m postgres-admin -c "/usr/lib/postgresql/10/bin/initdb --encoding=UTF-8 --locale=en_US.utf8 --lc-collate=C.UTF-8 --lc-ctype=C.UTF-8 -D /var/pgsql_data"
+printf "\nhost\tall\t\tpostgres-admin\tsamehost\t\tpassword\n" | tee -a /var/pgsql_data/pg_hba.conf
+
+apt-get purge -y --auto-remove wget
